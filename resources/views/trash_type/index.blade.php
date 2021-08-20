1dc01232d76a1a1201d1f583b93ffbdf29f9b0c4
Christian Bauer <development@bauer-siggenhofen.de>
2021-08-21 01:22:59 +0200

    adding blade for index view of trash types

---
+++ resources/views/trash_type/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layout.app')
+
+@section('content')
+    <div class="row justify-content-center">
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-header">M&uuml;llarten</div>
+                <div class="card-body">
+                    <a class="btn btn-sm btn-success float-end ms-2 mt-3 mb-3"><i class="fas fa-plus"></i> Neue Müllart anlegen</a>
+                    <table class="table table-sm">
+                        <tr>
+                            <th>#</th>
+                            <th>Art</th>
+                            <th>Farbgestaltung</th>
+                        </tr>
+                        @foreach($trash_types as $trash_type)
+                            <tr>
+                                <td><a href="/trash_types/{{$trash_type->id}}">{{ $trash_type->id }}</a></td>
+                                <td>{{ $trash_type->name }}</td>
+                                <td style="background-color: {{$trash_type->color}}; color: {{ $trash_type->text_color }};">{{ $trash_type->name }}</td>
+                            </tr>
+                        @endforeach
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
