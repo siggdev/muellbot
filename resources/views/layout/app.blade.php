e89a4e6355cb4622b7031408a720d5f0a539f7a7
Christian Bauer <development@bauer-siggenhofen.de>
2021-08-20 22:54:57 +0200

    adding basic app layout

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <!-- meta data
+        =============================================================================================================-->
+        <!-- set content type and charset -->
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta charset="UTF-8" />
+
+        <!-- set viewport for responsive design -->
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <!-- set author and description -->
+        <meta name="author" content="Christian Bauer" />
+        <meta name="description" content="Müllbot und -kalender" />
+
+        <!-- favicon -->
+        <!-- TODO: insert favicon -->
+
+
+        <!-- site title
+        =============================================================================================================-->
+        <title>Müllkalender</title>
+
+
+        <!-- style sheets
+        =============================================================================================================-->
+        <!-- bootstrap css file -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous" />
+
+        <!-- custom css file -->
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet" content="text/css" />
+    </head>
+    <body>
+
+
+
+        <!-- include bootstrap js -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+        <!-- include custom javascript -->
+        <script src="{{ asset('js/app.js') }}" content="text/javascript"></script>
+    </body>
+</html>
