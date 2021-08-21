1afbf098ee9c3a926df5ab30c2082984655a4e80
Christian Bauer <development@bauer-siggenhofen.de>
2021-08-21 12:45:51 +0200

    install jquery

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,10 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .js(
+        'node_modules/jquery/dist/jquery.js',
+        'public/js'
+    )
     .sass('resources/sass/app.scss', 'public/css')
     .copy(
         'node_modules/@fortawesome/fontawesome-free/webfonts',
